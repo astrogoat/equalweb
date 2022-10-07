642b2535daae88590b0634d8fdd892836a554c95
smolDev-ai <smolDev-ai@users.noreply.github.com>
2022-10-07 17:02:46 +0000

    Fix styling

--- src/EqualwebServiceProvider.php
+++ src/EqualwebServiceProvider.php
@@ -2,11 +2,11 @@
 
 namespace Astrogoat\Equalweb;
 
+use Astrogoat\Equalweb\Settings\EqualwebSettings;
 use Helix\Lego\Apps\App;
 use Helix\Lego\LegoManager;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Astrogoat\Equalweb\Settings\EqualwebSettings;
 
 class EqualwebServiceProvider extends PackageServiceProvider
 {
